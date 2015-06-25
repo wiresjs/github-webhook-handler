1e11eac5b4c791693576a4f27f960d4299ef0922
Ivan.Orlov <ivan@morrr.com>
2015-06-25 16:05:27 +0300

    Fix

--- github-webhook-handler.js
+++ github-webhook-handler.js
@@ -1,90 +1,96 @@
-const EventEmitter = require('events').EventEmitter
-    , inherits     = require('util').inherits
-    , crypto       = require('crypto')
-    , bl           = require('bl')
+const EventEmitter = require('events').EventEmitter,
+   inherits = require('util').inherits,
+   crypto = require('crypto'),
+   bl = require('bl')
 
 
-function signBlob (key, blob) {
-  return 'sha1=' + crypto.createHmac('sha1', key).update(blob).digest('hex')
+function signBlob(key, blob) {
+   return 'sha1=' + crypto.createHmac('sha1', key).update(blob).digest('hex')
 }
 
 
-function create (options) {
-  if (typeof options != 'object')
-    throw new TypeError('must provide an options object')
+function create(options) {
+   if (typeof options != 'object')
+      throw new TypeError('must provide an options object')
 
-  if (typeof options.path != 'string')
-    throw new TypeError('must provide a \'path\' option')
+   if (typeof options.path != 'string')
+      throw new TypeError('must provide a \'path\' option')
 
-  if (typeof options.secret != 'string')
-    throw new TypeError('must provide a \'secret\' option')
+   if (typeof options.secret != 'string')
+      throw new TypeError('must provide a \'secret\' option')
 
-  // make it an EventEmitter, sort of
-  handler.__proto__ = EventEmitter.prototype
-  EventEmitter.call(handler)
+   // make it an EventEmitter, sort of
+   handler.__proto__ = EventEmitter.prototype
+   EventEmitter.call(handler)
 
-  return handler
+   return handler
 
 
-  function handler (req, res, callback) {
-    if (req.url.split('?').shift() !== options.path)
-      return callback()
+   function handler(req, res, callback) {
+      if (req.url.split('?').shift() !== options.path)
+         return callback()
 
-    function hasError (msg) {
-      res.writeHead(400, { 'content-type': 'application/json' })
-      res.end(JSON.stringify({ error: msg }))
+      function hasError(msg) {
+         res.writeHead(400, {
+            'content-type': 'application/json'
+         })
+         res.end(JSON.stringify({
+            error: msg
+         }))
 
-      var err = new Error(msg)
+         var err = new Error(msg)
 
-      handler.emit('error', err, req)
-      callback(err)
-    }
-
-    var sig   = req.headers['x-hub-signature']
-      , event = req.headers['x-github-event']
-      , id    = req.headers['x-github-delivery']
-
-    if (!sig)
-      return hasError('No X-Hub-Signature found on request')
-
-    if (!event)
-      return hasError('No X-Github-Event found on request')
-
-    if (!id)
-      return hasError('No X-Github-Delivery found on request')
-
-    req.pipe(bl(function (err, data) {
-      if (err) {
-        return hasError(err.message)
+         handler.emit('error', err, req)
+         callback(err)
       }
 
-      var obj
+      var sig = req.headers['x-hub-signature'],
+         event = req.headers['x-github-event'],
+         id = req.headers['x-github-delivery']
 
-      if (sig !== signBlob(options.secret, data))
-        return hasError('X-Hub-Signature does not match blob signature')
+      if (!sig)
+         return hasError('No X-Hub-Signature found on request')
 
-      try {
-        obj = JSON.parse(data.toString())
-      } catch (e) {
-        return hasError(e)
-      }
+      if (!event)
+         return hasError('No X-Github-Event found on request')
 
-      res.writeHead(200, { 'content-type': 'application/json' })
-      res.end('{"ok":true}')
+      if (!id)
+         return hasError('No X-Github-Delivery found on request')
 
-      var emitData = {
-          event   : event
-        , id      : id
-        , payload : obj
-        , protocol: req.protocol
-        , host    : req.get('host')
-        , url     : req.url
-      }
+      req.pipe(bl(function(err, data) {
+         if (err) {
+            return hasError(err.message)
+         }
 
-      handler.emit(event, emitData)
-      handler.emit('*', emitData)
-    }))
-  }
+         var obj
+
+         if (sig !== signBlob(options.secret, data))
+            return hasError('X-Hub-Signature does not match blob signature')
+
+         try {
+            obj = JSON.parse(data.toString())
+         } catch (e) {
+            return hasError(e)
+         }
+
+         res.writeHead(200, {
+            'content-type': 'application/json'
+         })
+         res.end('{"ok":true}')
+
+         var emitData = {
+            event: event,
+            id: id,
+            payload: obj,
+            protocol: req.protocol,
+            host: 1,
+            url: 1
+         }
+
+         handler.emit(event, emitData)
+         handler.emit('*', emitData)
+      }))
+   }
 }
 
 
