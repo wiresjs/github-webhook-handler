331859f6a6881ffc397ea3b1adf1a3c2f8e45a14
Ivan.Orlov <ivan@morrr.com>
2015-06-25 16:27:34 +0300

    notifications

--- daemon.js
+++ daemon.js
@@ -4,6 +4,77 @@ var handler = createHandler({
    path: '/webhook',
    secret: 'myhashsecret'
 })
+var fs = require("fs");
+var path = require("path");
+var _ = require('lodash');
+var Promise = require('promise')
+var procstreams = require('procstreams');
+var Class = require("wires-class");
+var Stream = require('stream').Stream
+
+
+var Bash = Class.extend({
+   initialize: function(cmd) {
+      this.cmd = cmd ? [cmd] : [];
+   },
+   add: function() {
+      _.each(arguments, function(arg) {
+         this.cmd.push(arg);
+      }, this)
+   },
+   call: function(opts) {
+      var opts = opts || {};
+      var onStream = opts.onStream
+      var onError = opts.onError;
+      var pipes = opts.pipes || [];
+      var ignoreErrors = opts.ignoreErrors;
+      var printOutput = opts.printOutput;
+      var self = this;
+      return new Promise(function(resolve, reject) {
+         var stream = new Stream()
+         stream.writable = true
+         stream.read = onStream
+         stream.write = function(a) {
+            if (printOutput) {
+               console.log(a.toString().trim())
+            }
+            onStream ? onStream(a.toString()) : null;
+         }
+         stream.end = function() {}
+
+
+         var errStream = new Stream()
+         errStream.writable = true
+         errStream.write = function(a) {
+            onError ? onError(a.toString()) : null;
+         }
+         errStream.end = function() {}
+         var fullcmd = self.cmd.join(' ');
+
+
+         var proc = procstreams(fullcmd).data(function(err, stdout, stderr) {
+            if (err) {
+               if (ignoreErrors) {
+                  return resolve();
+               }
+               return reject({
+                  err: err,
+                  out: (stderr ? stderr.toString() : undefined)
+               });
+            }
+            return resolve(stdout ? stdout.toString().split("\n") : undefined)
+         })
+         _.each(pipes, function(item) {
+            proc = proc.pipe(item)
+         })
+         proc.pipe(stream, {
+            stderr: errStream
+         })
+      });
+   }
+})
+
+
 
 http.createServer(function(req, res) {
    handler(req, res, function(err) {
@@ -16,13 +87,35 @@ handler.on('error', function(err) {
    console.error('Error:', err.message)
 })
 
+var current = process.cwd();
+var config = JSON.parse(fs.readFileSync(path.join(current, "github.json")));
+
 handler.on('push', function(event) {
-   console.log(JSON.stringify(event, 2, 2));
-   console.log('Received a push event for %s to %s',
-      event.payload.repository.name,
-      event.payload.ref)
+   for (var repName in config) {
+      if (repName === event.payload.repository.name) {
+         var branches = config[repName];
+         for (var branch in branches) {
+            if (event.payload.ref.indexOf(branch) > -1) {
+               var cmd = branches[branch]
+               var bash = new Bash();
+               bash.add(cmd)
+               bash.call({
+                  printOutput: true
+               }).then(function(d) {
+                  console.log(d)
+               }).catch(function(e) {
+                  console.log(e)
+               })
+
+            }
+         }
+      }
+   }
+   var repositoryName = event.payload.repository.name;
+   var branch = event.payload.ref;
 })
 
+/*
 handler.on('issues', function(event) {
    console.log('Received an issue event for %s action=%s: #%d %s',
       event.payload.repository.name,
@@ -30,3 +123,5 @@ handler.on('issues', function(event) {
       event.payload.issue.number,
       event.payload.issue.title)
 })
+
+*/
